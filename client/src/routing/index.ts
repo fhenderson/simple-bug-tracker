26a4abceb77902668864812c197f9ae508b34b12
fhenderson <francois.henderson@gmail.com>
2020-09-22 10:49:40 -0500

    Adding page not founds to routings

---
+++ client/src/routing/index.ts
@@ -0,0 +1,13 @@
+import PATH_CODES from './pathnames'
+import PrivateRoute from './PrivateRoute'
+import StandardLayoutRoute from './StandardLayoutRoute'
+import Routes from './Routes/Routes'
+import ErrorLayoutRoute from './ErrorLayoutRoute/ErrorLayoutRoute'
+
+export {
+  PATH_CODES,
+  PrivateRoute,
+  StandardLayoutRoute,
+  Routes,
+  ErrorLayoutRoute
+}
